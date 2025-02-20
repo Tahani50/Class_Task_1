966ea8f3421a0db1dedb3b471096ea749c0f3144
Tahani <tahanayman4@gmail.com>
2025-02-20 16:40:05 +0300

    task

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ var isActive = true;
 var data = 42;
 var iput;
 var user = "John";
+var number = [1, 2, 3, 4,]; //arrays
+var person = ["Tahani", 24]; //tuples
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 0] = "admin";
+    Role[Role["user"] = 1] = "user";
+    Role[Role["guest"] = 2] = "guest";
+})(Role || (Role = {}));
+; // enums
+var myRole = Role.admin;
 //Function
 function add(a, b) {
     return a + b;
@@ -20,3 +30,31 @@ var MyCar = {
     year: 2022,
 };
 console.log("".concat(MyCar.brand, " was made in ").concat(MyCar.year));
+// interface User {
+//     name: string;
+//     age: number;
+//     isAdmin?: boolean; // Optional property
+//   }
+//const username: User = { name: "Fatima", age: 30 };
+//Using Interfaces in Functions
+//   function printUser(username: User) {
+//     console.log(`${username.name} is ${username.age} years old`);
+//   }
+//Call the function to display output
+//printUser(username);
+//classes
+var Car2 = /** @class */ (function () {
+    function Car2(brand) {
+        this.brand = brand;
+    }
+    Car2.prototype.drive = function () {
+        console.log("My car brand is ".concat(MyCar.brand));
+    };
+    return Car2;
+}());
+// generic
+function identity(value) {
+    return value;
+}
+console.log(identity("Hello")); // Output: "Hello"
+console.log(identity(123)); // Output: 123
--- task.ts
+++ task.ts
@@ -1,22 +1,17 @@
 // Task 1: Implement a Utility Function
 function capitalizeWords(str: string):
 string {
-    if(str == ""){
+    if(str === ""){
         return "The String is empty";
     }else{
-        return str.toUpperCase();
+        return str.charAt(0).toUpperCase() + str.slice(1);
     }
 }
 
 // Task 2: Number Validator Function
 function isValidNumber(value: unknown):
 boolean{
-    if(typeof value === "number"){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return typeof value === 'number' && !isNaN(value);
 }
 
 // Task 3: Create a User System
@@ -38,9 +33,9 @@ interface User {
 
   // Task 4: Implement a Product Class
   class Product{
-    name: string;
-    price: number;
-    category: string;
+    public name: string;
+    private price: number;
+    public category: string;
 
     constructor(name: string, price: number, category: string){
         this.name = name;
@@ -49,7 +44,7 @@ interface User {
     }
      
     getDiscountedPrice(discount: number){ 
-        return this.price - discount;
+        return this.price - (this.price * discount) / 100;
     }
   }
 
@@ -60,11 +55,12 @@ interface User {
   }
   
   // Task 6: Fetch & Display Users
-  async function fetchEmployees(apiUrl: string): Promise<User[]> {
+  async function fetchUsers(): Promise<User[]> {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    return data;
+   const data = await response.json();
+    return data.map((user: any) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    }));
 }
-
-
-
